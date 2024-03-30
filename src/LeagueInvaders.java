443b82b142a2ee8de8d6dd63c4c9f8a757d05544
league <ashkonsaeidi@gmail.com>
2024-03-30 16:30:52 -0700

    Continue the league invaders java thingie

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,20 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LeagueInvaders invader = new LeagueInvaders();
+	}
+	JFrame frame = new JFrame();
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	public LeagueInvaders() {
+		frame.getMaximumSize();
+	}
+	void setup() {
+		WIDTH = 500;
+		HEIGHT = 800;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
 }
