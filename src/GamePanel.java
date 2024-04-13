e6c5334edd46993cd63c630c807d6a22bd354a59
league <aaritrj@gmail.com>
2024-04-13 16:26:24 -0700

    Finish part 1 of the space invaders thingie uk what to do

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
@@ -8,6 +9,13 @@ public class GamePanel extends JPanel{
 	final int GAME = 1; 
 	final int END = 2;
 	int currentState = MENU;
+	Font titleFont;
+	
+	GamePanel GamePanel = new GamePanel();
+	
+	public GamePanel(){
+		titleFont = new Font("Arial", Font.PLAIN, 48);
+	}
 	
 	void updateMenuState() {
 		
@@ -24,14 +32,19 @@ public class GamePanel extends JPanel{
 	void drawMenuState(Graphics g) {
 		g.setColor(Color.BLUE);
 		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("League Invaders", 200, 100);
 	}
 	
 	void drawGameState(Graphics g) {
-		
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
 	}
 	
 	void drawEndState(Graphics g) {
-	
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
 	}
 	
 	@Override
