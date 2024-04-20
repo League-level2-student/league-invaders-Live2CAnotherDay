dc979483e9dad5bcbc249e90c56ceb33ee90f677
league <ashkonsaeidi@gmail.com>
2024-04-20 16:31:09 -0700

    finish 2nd part of leauge invaders

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,20 +1,26 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements ActionListener{
 	
 	final int MENU = 0;
 	final int GAME = 1; 
 	final int END = 2;
 	int currentState = MENU;
 	Font titleFont;
-	
-	GamePanel GamePanel = new GamePanel();
+	Font normalFont;
+	Timer frameDraw;
 	
 	public GamePanel(){
 		titleFont = new Font("Arial", Font.PLAIN, 48);
+		normalFont = new Font("Arial", Font.PLAIN, 24);
+		frameDraw = new Timer(1000/6, this);
+		frameDraw.start();
 	}
 	
 	void updateMenuState() {
@@ -34,7 +40,10 @@ public class GamePanel extends JPanel{
 		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
 		g.setFont(titleFont);
 		g.setColor(Color.YELLOW);
-		g.drawString("League Invaders", 200, 100);
+		g.drawString("League Invaders", 75, 100);
+		g.setFont(normalFont);
+		g.drawString("Press ENTER to start", 125, 250);
+		g.drawString("Press SPACE for instructions", 90, 550);
 	}
 	
 	void drawGameState(Graphics g) {
@@ -45,6 +54,9 @@ public class GamePanel extends JPanel{
 	void drawEndState(Graphics g) {
 		g.setColor(Color.RED);
 		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.CYAN);
+		g.drawString("GAME OVER", 175, 100);
 	}
 	
 	@Override
@@ -56,5 +68,20 @@ public class GamePanel extends JPanel{
 		}else if(currentState == END) {
 			drawEndState(g);
 		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(currentState == MENU) {
+			updateMenuState();
+		}else if(currentState == GAME) {
+			updateGameState();
+		}else if(currentState == END) {
+			updateEndState();
+		}
+		System.out.println("action");
+		repaint();
+		
 	}
 }
