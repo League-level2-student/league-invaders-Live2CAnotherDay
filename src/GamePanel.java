61c986f22285aa4548bbae5d2aa316e144afe20d
league <aaritrj@gmail.com>
2024-04-27 16:27:10 -0700

    Continue working on part 3 of rocketship movement

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,10 +3,13 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements ActionListener{
+public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	
 	final int MENU = 0;
 	final int GAME = 1; 
@@ -15,12 +18,14 @@ public class GamePanel extends JPanel implements ActionListener{
 	Font titleFont;
 	Font normalFont;
 	Timer frameDraw;
+	Rocketship rocketship = new Rocketship(250, 700, 50, 50);
 	
 	public GamePanel(){
 		titleFont = new Font("Arial", Font.PLAIN, 48);
 		normalFont = new Font("Arial", Font.PLAIN, 24);
-		frameDraw = new Timer(1000/6, this);
+		frameDraw = new Timer(1000/60, this);
 		frameDraw.start();
+		
 	}
 	
 	void updateMenuState() {
@@ -49,6 +54,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	void drawGameState(Graphics g) {
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		rocketship.draw(g);
 	}
 	
 	void drawEndState(Graphics g) {
@@ -56,7 +62,10 @@ public class GamePanel extends JPanel implements ActionListener{
 		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
 		g.setFont(titleFont);
 		g.setColor(Color.CYAN);
-		g.drawString("GAME OVER", 175, 100);
+		g.drawString("GAME OVER", 100, 100);
+		g.setFont(normalFont);
+		g.drawString("You killed enemies", 145, 250);
+		g.drawString("Press ENTER to restart", 130, 550);
 	}
 	
 	@Override
@@ -80,8 +89,55 @@ public class GamePanel extends JPanel implements ActionListener{
 		}else if(currentState == END) {
 			updateEndState();
 		}
-		System.out.println("action");
+		//System.out.println("action");
 		repaint();
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		if(arg0.getKeyCode() == KeyEvent.VK_ENTER) {
+			if(currentState == END) {
+				currentState = MENU;
+			}
+			else {
+				currentState++;
+			}
+		}
+		if(arg0.getKeyCode() == KeyEvent.VK_UP) {	
+			if (rocketship.y > 0) {
+			System.out.println("UP");
+			rocketship.up();
+			}
+		}else if(arg0.getKeyCode() == KeyEvent.VK_DOWN) {			
+			if (rocketship.y < LeagueInvaders.HEIGHT -rocketship.height) {
+				System.out.println("DOWN");
+				rocketship.down();
+				}
+		}else if(arg0.getKeyCode() == KeyEvent.VK_RIGHT) {			
+			if (rocketship.x < LeagueInvaders.WIDTH -rocketship.width) {
+				System.out.println("RIGHT");
+				rocketship.right();
+				}
+		}else if(arg0.getKeyCode() == KeyEvent.VK_LEFT) {			
+			if (rocketship.x > 0) {
+				System.out.println("LEFT");
+				rocketship.left();
+				}
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 }
